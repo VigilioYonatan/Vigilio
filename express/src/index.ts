5bc516d09e479d16fbb495d840e55ddc884b5915
Vigilio <yonatanvigiliolavado09@gmail.com>
2023-07-19 14:49:09 -0500

    change build

--- express/src/index.ts
+++ express/src/index.ts
@@ -1,6 +1,5 @@
 import { Pipe } from "./decorators/pipe.decorator";
 import { Upload } from "./decorators/upload.decorator";
 import { Validator } from "./decorators/validator.decorator";
-import * as handler from "./handler";
 
-export default { ...handler, Pipe, Upload, Validator };
+export { Pipe, Upload, Validator };
