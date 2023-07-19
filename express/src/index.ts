f0555e70541b9ca86fb03ff89dd20d63e2464497
Vigilio <yonatanvigiliolavado09@gmail.com>
2023-07-19 16:11:08 -0500

    change package.json

--- express/src/index.ts
+++ express/src/index.ts
@@ -1,6 +1,6 @@
 import { Pipe } from "./decorators/pipe.decorator";
 import { Upload } from "./decorators/upload.decorator";
 import { Validator } from "./decorators/validator.decorator";
-import "./helpers";
-import "./handler";
+export * from "./helpers";
+export * from "./handler";
 export { Pipe, Upload, Validator };
