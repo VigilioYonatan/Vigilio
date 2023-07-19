27a3546f6ce7cd71979631ac80db73743cdc30a3
Vigilio <yonatanvigiliolavado09@gmail.com>
2023-07-19 17:07:55 -0500

    gaa

--- express/src/index.ts
+++ express/src/index.ts
@@ -1,4 +1,6 @@
 import { Pipe } from "./decorators/pipe.decorator";
 import { Upload } from "./decorators/upload.decorator";
 import { Validator } from "./decorators/validator.decorator";
+export * from "./helpers";
+export * from "./handler";
 export { Pipe, Upload, Validator };
