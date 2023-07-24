d0ff36662b0b7061ec9815dd25bab1d5871a45f7
Vigilio <yonatanvigiliolavado09@gmail.com>
2023-07-24 02:00:53 -0500

    add vite

---
+++ express/src/client/index.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+import { vite } from "./vite";
+interface Client {
+    file?: string;
+    port?: number;
+    host?: string;
+}
+export function client(props: Client | undefined) {
+    const {
+        file = "main.ts",
+        port = 3000,
+        host = "http://localhost:4000",
+    } = props || { file: "main.ts", host: "http://localhost:4000", port: 3000 };
+    return async (_req: Request, res: Response, next: NextFunction) => {
+        const vites = await vite(file, "http://localhost:" + port);
+        const route = (uri: string) => `${host}${uri}`;
+        res.locals.vite = vites;
+        res.locals.route = route;
+        next();
+    };
+}
---
+++ express/src/client/vite.ts
@@ -0,0 +1,103 @@
+export async function vite(entry: string, host: string) {
+    return (
+        "\n" +
+        (await jsTag(entry, host)) +
+        "\n" +
+        (await jsPreloadImports(entry, host)) +
+        "\n" +
+        (await cssTag(entry, host))
+    );
+}
+
+async function isDev(entry: string, host: string) {
+    const exists = await checkViteEntry(entry, host);
+    return exists;
+}
+
+async function checkViteEntry(entry: string, host: string) {
+    const url = host + "/" + entry;
+    try {
+        const response = await fetch(url, { method: "HEAD" });
+        return response.ok;
+    } catch (error) {
+        return false;
+    }
+}
+
+async function jsTag(entry: string, host: string) {
+    const url = (await isDev(entry, host))
+        ? host + "/" + entry
+        : assetUrl(entry);
+
+    if (!url) {
+        return "";
+    }
+    return `<script type="module" crossorigin src="${url}"></script>`;
+}
+
+async function jsPreloadImports(entry: string, host: string) {
+    if (await isDev(entry, host)) {
+        return "";
+    }
+
+    let res = "";
+    const urls = importsUrls(entry);
+    for (const url of urls) {
+        res += `<link rel="modulepreload" href="${url}">`;
+    }
+    return res;
+}
+
+async function cssTag(entry: string, host: string) {
+    if (await isDev(entry, host)) {
+        return "";
+    }
+
+    let tags = "";
+    const urls = cssUrls(entry);
+    for (const url of urls) {
+        tags += `<link rel="stylesheet" href="${url}">`;
+    }
+    return tags;
+}
+
+function getManifest() {
+    const fs = require("fs");
+    const path = require("path");
+    const manifestPath = path.join(
+        __dirname,
+        "/../../public/dist/manifest.json"
+    );
+    const content = fs.readFileSync(manifestPath, "utf-8");
+    return JSON.parse(content);
+}
+
+function assetUrl(entry: string) {
+    const manifest = getManifest();
+
+    return manifest[entry] ? "/dist/" + manifest[entry]["file"] : "";
+}
+
+function importsUrls(entry: string) {
+    const urls = [];
+    const manifest = getManifest();
+
+    if (manifest[entry]["imports"] && manifest[entry]["imports"].length > 0) {
+        for (const imports of manifest[entry]["imports"]) {
+            urls.push("/dist/" + manifest[imports]["file"]);
+        }
+    }
+    return urls;
+}
+
+function cssUrls(entry: string) {
+    const urls = [];
+    const manifest = getManifest();
+
+    if (manifest[entry]["css"] && manifest[entry]["css"].length > 0) {
+        for (const file of manifest[entry]["css"]) {
+            urls.push("/dist/" + file);
+        }
+    }
+    return urls;
+}
