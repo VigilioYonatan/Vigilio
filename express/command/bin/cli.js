23f2db3dd8de26e1e2cc511b3b7b4e5abdb1da1d
Vigilio <yonatanvigiliolavado09@gmail.com>
2023-07-29 11:10:32 -0500

    fixed

--- express/command/bin/cli.js
+++ express/command/bin/cli.js
@@ -22,26 +22,26 @@ inquirer
                         choices: ["fullstack", "api"],
                     })
                     .then((result) => {
-                        let pack = null;
+                        let packs = null;
                         switch (result.type) {
                             case "fullstack":
-                                pack = `git clone --depth 1 https://github.com/VigilioYonatan/express-api ${projectName}`;
+                                packs = `git clone --depth 1 https://github.com/VigilioYonatan/express-api ${projectName}`;
                                 break;
                             case "api":
-                                pack = `git clone --depth 1 https://github.com/VigilioYonatan/vigilio-express ${projectName}`;
+                                packs = `git clone --depth 1 https://github.com/VigilioYonatan/vigilio-express ${projectName}`;
                                 break;
                             default:
                                 break;
                         }
+                        const installPackageNpm = `cd ${projectName} && ${pack.package} install`;
+                        console.log("cloning project");
+                        const clonning = runCommand(packs);
+                        if (!clonning) process.exit(-1);
+                        console.log(`Installing packages ${pack.package}`);
+                        const installPackage = runCommand(installPackageNpm);
+                        if (!installPackage) process.exit(-1);
+                        console.log("Congratulations...");
                     });
-                const installPackageNpm = `cd ${projectName} && ${pack.package} install`;
-                console.log("cloning project");
-                const clonning = runCommand(pack);
-                if (!clonning) process.exit(-1);
-                console.log(`Installing packages ${pack.package}`);
-                const installPackage = runCommand(installPackageNpm);
-                if (!installPackage) process.exit(-1);
-                console.log("Congratulations...");
             });
     });
 
