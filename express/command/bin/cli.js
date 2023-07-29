0f498e6212f7a5da3e89e0a95cb2267c31292c69
Vigilio <yonatanvigiliolavado09@gmail.com>
2023-07-29 02:16:12 -0500

    change cli

--- express/command/bin/cli.js
+++ express/command/bin/cli.js
@@ -14,10 +14,29 @@ inquirer
                 choices: ["npm", "yarn", "pnpm"],
             })
             .then((pack) => {
-                const cloning = `git clone --depth 1 `;
+                inquirer
+                    .prompt({
+                        type: "list",
+                        name: "type",
+                        message: "select @express/vigilio package",
+                        choices: ["fullstack", "api"],
+                    })
+                    .then((result) => {
+                        let pack = null;
+                        switch (result) {
+                            case "fullstack":
+                                pack = `git clone --depth 1 https://github.com/VigilioYonatan/express-api ${projectName}`;
+                                break;
+                            case "api":
+                                pack = `git clone --depth 1 https://github.com/VigilioYonatan/vigilio-express ${projectName}`;
+                                break;
+                            default:
+                                break;
+                        }
+                    });
                 const installPackageNpm = `cd ${projectName} && ${pack.package} install`;
                 console.log("cloning project");
-                const clonning = runCommand(cloning);
+                const clonning = runCommand(pack);
                 if (!clonning) process.exit(-1);
                 console.log(`Installing packages ${pack.package}`);
                 const installPackage = runCommand(installPackageNpm);
