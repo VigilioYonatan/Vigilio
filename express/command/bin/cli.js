955fcfd0dee70bdca495a24ab288be2e79450ba3
Vigilio <yonatanvigiliolavado09@gmail.com>
2023-07-29 02:01:00 -0500

    fix cli

--- express/command/bin/cli.js
+++ express/command/bin/cli.js
@@ -14,52 +14,15 @@ inquirer
                 choices: ["npm", "yarn", "pnpm"],
             })
             .then((pack) => {
-                inquirer
-                    .prompt({
-                        type: "list",
-                        name: "opcion",
-                        message: "select a framework",
-                        choices: ["vanilla", "vue", "react", "preact"],
-                    })
-                    .then(async (result) => {
-                        let command = null;
-                        let change = null;
-                        switch (result.opcion) {
-                            case "vanilla":
-                                command = await getRepository(
-                                    null,
-                                    `git clone --depth 1 https://github.com/VigilioYonatan/vigilio-express ${projectName} `
-                                );
-                                break;
-                            case "vue":
-                                command = await getRepository(
-                                    null,
-                                    `git clone  https://github.com/VigilioYonatan/vigilio-express.git ${projectName}`
-                                );
-                                change = "vue-ts";
-                                break;
-                            case "react":
-                                break;
-
-                            case "preact":
-                                break;
-
-                            default:
-                                break;
-                        }
-                        console.log("download project");
-                        const printCommmand = runCommand(command);
-                        if (!printCommmand) process.exit(-1);
-                        console.log("Installing package dependencies");
-                        const printCommmand2 = runCommand(
-                            `cd ${projectName} && ${
-                                change ? `git checkout ${change} &&` : ""
-                            } ${pack.package} install`
-                        );
-                        if (!printCommmand2) process.exit(-1);
-                        runCommand("rimraf .git/");
-                        console.log("Finished project");
-                    });
+                const cloning = `git clone --depth 1 `;
+                const installPackageNpm = `cd ${projectName} && ${pack.package} install`;
+                console.log("cloning project");
+                const clonning = runCommand(cloning);
+                if (!clonning) process.exit(-1);
+                console.log(`Installing packages ${pack.package}`);
+                const installPackage = runCommand(installPackageNpm);
+                if (!installPackage) process.exit(-1);
+                console.log("Congratulations...");
             });
     });
 
@@ -71,19 +34,4 @@ function runCommand(command) {
         return false;
     }
     return true;
-}
-async function getRepository(js, ts) {
-    let command = null;
-    const result = await inquirer.prompt({
-        type: "list",
-        name: "language",
-        message: "choose language programming",
-        choices: ["javascript", "typescript"],
-    });
-    if (result.language === "javascript") {
-        command = js;
-        return;
-    }
-    command = ts;
-    return command;
 }
