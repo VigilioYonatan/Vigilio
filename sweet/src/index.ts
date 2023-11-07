82d494d8042427aa7a49c8e3cca12594ce359b13
Vigilio <yonatanvigiliolavado09@gmail.com>
2023-11-07 01:02:10 -0500

    fixed

--- sweet/src/index.ts
+++ sweet/src/index.ts
@@ -1,6 +1,6 @@
 import sweetModal from "./sweet";
-import sweelAlert from "./alert";
+import sweetAlert from "./alert";
 import { h as c } from "./helpers";
 import { type Icon, type SwalProps, type SwalAlertProps } from "./types";
 export { Icon, SwalProps, c, SwalAlertProps };
-export  { sweetModal, sweelAlert };
+export { sweetModal, sweetAlert };
