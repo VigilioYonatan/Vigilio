54ed901a6e81a0a61200ddb807735e154f78b6ac
Vigilio <yonatanvigiliolavado09@gmail.com>
2024-01-06 11:20:07 -0500

    i18NEXT ADD

---
+++ express/src/i18next/index.ts
@@ -0,0 +1,17 @@
+import { InterpolationMap, TOptionsBase } from "i18next";
+
+type Join<S1, S2> = S1 extends string
+    ? S2 extends string
+        ? `${S1}.${S2}`
+        : never
+    : never;
+
+export type Paths<T> = {
+    [K in keyof T]: T[K] extends Record<string, unknown>
+        ? Join<K, Paths<T[K]>>
+        : K;
+}[keyof T];
+export type I18nextFunction<T extends object> = (
+    s: Paths<T> | TemplateStringsArray | (string | TemplateStringsArray)[],
+    options?: (TOptionsBase & InterpolationMap<string>) | undefined
+) => string;
