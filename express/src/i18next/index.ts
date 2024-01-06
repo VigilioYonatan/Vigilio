521ae3d24a854f5819c1f283c3c30df0ea28e1c4
Vigilio <yonatanvigiliolavado09@gmail.com>
2024-01-06 17:55:24 -0500

    sadasd

--- express/src/i18next/index.ts
+++ express/src/i18next/index.ts
@@ -11,7 +11,12 @@ export type Paths<T> = {
         ? Join<K, Paths<T[K]>>
         : K;
 }[keyof T];
-export type I18nextFunction<T extends object> = (
-    s: Paths<T> | TemplateStringsArray | (string | TemplateStringsArray)[],
+
+export type I18nextFunction<T extends Record<string, any>> = (
+    s: T extends Record<string, any>
+        ? {
+              [K in keyof T]: `${K & string}:${Paths<T[K & string]>}`;
+          }[keyof T]
+        : "" | TemplateStringsArray | (string | TemplateStringsArray)[],
     options?: (TOptionsBase & InterpolationMap<string>) | undefined
 ) => string;
