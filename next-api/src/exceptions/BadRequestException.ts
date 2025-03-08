b70c267cd89e5b2baaa09016300898da422f8aab
vigilio <yonatanvigiliolavado09@gmail.com>
2025-03-08 15:58:52 -0500

    newchanges

--- next-api/src/exceptions/BadRequestException.ts
+++ next-api/src/exceptions/BadRequestException.ts
@@ -1,18 +1,21 @@
 import { HttpException } from "./HttpException";
 
 export class BadRequestException extends HttpException {
-	public name = "BadRequestException";
+    public name = "BadRequestException";
 
-	/**
-	 * Instantiates a `BadRequestException` Exception with status code 400.
-	 *
-	 * @param message Error message (default: 'Bad Request')
-	 * @param errors Additional errors
-	 *
-	 * @example
-	 * `throw new BadRequestException()`
-	 */
-	public constructor(message = "Bad Request", errors?: string[]) {
-		super(400, message, errors);
-	}
+    /**
+     * Instantiates a `BadRequestException` Exception with status code 400.
+     *
+     * @param message Error message (default: 'Bad Request')
+     * @param errors Additional errors
+     *
+     * @example
+     * `throw new BadRequestException()`
+     */
+    public constructor(
+        message = "Bad Request",
+        errors?: Record<string, unknown>
+    ) {
+        super(400, message, errors);
+    }
 }
--- next-api/src/exceptions/HttpException.ts
+++ next-api/src/exceptions/HttpException.ts
@@ -1,24 +1,28 @@
 /** Defines the base HTTP exception. */
 export class HttpException extends Error {
-	public name = "HttpException";
-	public statusCode: number;
-	public errors?: string[];
-	public isSuccess = false;
+    public name = "HttpException";
+    public statusCode: number;
+    public errors?: Record<string, unknown>;
+    public isSuccess = false;
 
-	/**
-	 * Instantiates an `HttpException` Exception.
-	 *
-	 * @param statusCode HTTP status code
-	 * @param message Error message
-	 * @param errors Additional errors
-	 *
-	 * @example
-	 * `throw new HttpException(403, 'Forbidden')`
-	 */
-	public constructor(statusCode: number, message?: string, errors?: string[]) {
-		super(message);
-		this.statusCode = statusCode;
-		this.errors = errors;
-		this.isSuccess = false;
-	}
+    /**
+     * Instantiates an `HttpException` Exception.
+     *
+     * @param statusCode HTTP status code
+     * @param message Error message
+     * @param errors Additional errors
+     *
+     * @example
+     * `throw new HttpException(403, 'Forbidden')`
+     */
+    public constructor(
+        statusCode: number,
+        message?: string,
+        errors?: Record<string, unknown>
+    ) {
+        super(message);
+        this.statusCode = statusCode;
+        this.errors = errors;
+        this.isSuccess = false;
+    }
 }
